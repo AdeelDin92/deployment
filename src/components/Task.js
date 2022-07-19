00d33d9e24dacc4d85f990fe736182e535035b74
ADEEL <AdeelDin@outlook.de>
2022-07-19 11:06:05 +0200

    updated

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,5 +1,5 @@
 import React,{useState} from "react";
-import { useNavigate,useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import RegionsList from "./RegionsList";
@@ -8,7 +8,7 @@ import RegionsList from "./RegionsList";
 function Task() {
   let naviagate = useNavigate();
   const [isActive,setActive] = useState("false");
-  const {workerId, campaignId} = useParams();
+ 
 
   /* Navigate to Tutorial  */
   const openTutorial = () => {
@@ -21,11 +21,11 @@ function Task() {
       <div className="d-flex justify-content-between mt-3">
         <p style={{"visibility":"hidden"}}>extra</p>
         <h2 className="d-inline-block">Annotation Task</h2> 
-        <h5 onClick={openTutorial} style={{"float":"right","marginRight":"10px"}}>Tutorial <img src="/logo.png" style={{"cursor":"pointer"}}></img></h5>     
+        <h5 onClick={openTutorial} style={{"float":"right","marginRight":"10px"}}>Tutorial <img src="/logo.png" alt="opentutorial" style={{"cursor":"pointer"}}></img></h5>     
              
        
         <div className={isActive ? "tutorial" : "tt"}> 
-          <h5 style={{"float":"right" ,"marginRight":"20px"}} onClick={openTutorial}>Tutorial<img src ="/logo.png" style={{"cursor":"pointer"}}></img> </h5>     
+          <h5 style={{"float":"right" ,"marginRight":"20px"}} onClick={openTutorial}>Tutorial<img src ="/logo.png" style={{"cursor":"pointer"}} alt="opentutorial2" ></img> </h5>     
           <div className="row justify-content-evenly align-items-center" style={{height:"80vh "}}>
           <div className="col-3 ">
             <img src="/Slideshow/image_0.gif" alt = "show1" style={{width:"100%"}}></img>
