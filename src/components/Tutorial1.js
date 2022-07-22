5f586f38000191add1bc50169a9e86debd8ec613
ADEEL <AdeelDin@outlook.de>
2022-07-22 10:51:54 +0200

    latest

--- src/components/Tutorial1.js
+++ src/components/Tutorial1.js
@@ -1,31 +1,33 @@
 import React, { useState } from "react";
 
-import { useNavigate,useParams} from "react-router-dom";
+import { useNavigate,useParams } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function Tutorial1() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(1);
+  const[imgSrc,setimgSrc] = useState(0)
   const [disabled, setDisabled] = useState(true);
   const {workerId, campaignId} = useParams();
-  
-function increment() {
-    if (count <= 4){
 
+  function increment() {
+    if (imgSrc <= 3){
         setCount(count+1)
+        setimgSrc(imgSrc +1)
         let lmg = document.querySelector(".img");
          // eslint-disable-next-line
-        let g = `/Slideshow/image_${count + 1}` + ".gif";
+        let g = `/Slideshow/image_${imgSrc + 1}` + ".gif";
         lmg.src = g;
         setDisabled(false);
+        console.log(count)
     }
 
-        if (count === 2) {
+        if (count === 3) {
+          setCount(count+1)
           let nextBtn = document.querySelector(".sum");
           console.log(count);
           nextBtn.innerHTML = "Continue";
         }
-        if (count === 3) {
-          console.log("hey");
+        if (count === 4) {         
           let lmg = document.querySelector(".img");
           lmg.src = "/Slideshow/image_0.gif";
           navigate(`/${workerId}/${campaignId}/Test`);
@@ -34,20 +36,22 @@ function increment() {
   }
 
   function decrement() {
-    if (count >= 0){
+    if (count >= 1){
       setCount(count-1)
+      setimgSrc(imgSrc - 1)
       let lmg = document.querySelector(".img");
       // eslint-disable-next-line
-     let g = `/Slideshow/image_${count- 1}` + `.gif`;
+     let g = `/Slideshow/image_${imgSrc- 1}` + `.gif`;
      lmg.src = g;
      let nextBtn = document.querySelector(".sum");
      nextBtn.innerHTML = "Next";
      setDisabled(false);
+     
 
     }
      
        
-        if (count === 1) {
+        if (count === 2) {
           setDisabled(true);
         }
       ;
@@ -60,8 +64,8 @@ function increment() {
         className="container"
         style={{ backgroundColor: "#FFFFFF", width: "100%", height: "100vh" }}
       >
-        <h1 className="text-center pt-5 mb-5 fw-bold">Slideshow</h1>
-        <p className="text-center">Showing {count}/3</p>
+        <h1 className="text-center pt-5 mb-5 fw-bold">Tutorial</h1>
+        <p className="text-center">Showing {count}/4</p>
         <div className="container">
           <div
             className="col-6 mx-auto"
@@ -69,7 +73,7 @@ function increment() {
           >
             <img
               src="/Slideshow/image_0.gif"
-              alt="show"
+              alt="show2"
               className="img-fluid img"
               style={{ width: "100%", height: "100%" }}
             ></img>
