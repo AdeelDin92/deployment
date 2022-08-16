1aac1561384e8c60b049ba5425ad34f5d395461b
ADEEL <AdeelDin@outlook.de>
2022-08-16 14:05:33 +0200

    Change Slides

--- src/components/Testing/SlideTwo.js
+++ src/components/Testing/SlideTwo.js
@@ -11,19 +11,20 @@ function SlideTwo({TestAnswers,setTestanswers}){
     setTestanswers({ ...TestAnswers, [name]: value });
    
   };
+  //"/Testing/Test2_No_Tumor.jpg"
   return(
   <div>
       <div className="Container">
           <div className="row justify-content-around py-4">
             <div className="col-sm-6 col-md-6 col-lg-5 col-xxl-6 col-xl-5">
-              <img src="/Testing/Test2_Tumor.jpg"  alt="tumor" className="card-img-size img-fluid" ></img>
+              <img src="/Testing/Test2_No_Tumor.jpg"  alt="tumor" className="card-img-size img-fluid" ></img>
               <div className="d-flex justify-content-center my-5">
                 <input type="radio" className="ms-3" name="Q2" value="A" onChange={handleTestAnswers} ></input>
                 <label>A</label>
               </div>
             </div>
             <div className="col-sm-6 col-md-6 col-lg-5 col-xxl-6 col-xl-5">
-              <img src="/Testing/Test2_No_Tumor.jpg" alt="no tumor" className="card-img-size img-fluid" ></img>
+              <img src="/Testing/Test2_Tumor.jpg" alt="no tumor" className="card-img-size img-fluid" ></img>
               <div className="d-flex justify-content-center my-5">
                 <input type="radio" className="ms-3" name="Q2" value="B" onChange={handleTestAnswers}></input>
                 <label>B</label>
